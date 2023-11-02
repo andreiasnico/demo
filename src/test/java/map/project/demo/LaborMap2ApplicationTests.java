f951507b9434b104fe3dc9fc8638776fca400142
zsoltrapolti <zsoltikarap@gmail.com>
2023-11-02 13:32:06 +0200

    Test for units and bill

--- src/test/java/map/project/demo/LaborMap2ApplicationTests.java
+++ src/test/java/map/project/demo/LaborMap2ApplicationTests.java
@@ -2,12 +2,26 @@ package map.project.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import map.project.demo.Model.Bill;
+import map.project.demo.Model.DeliveryMethods;
+import map.project.demo.Model.PaymentStatus;
+import map.project.demo.Model.Unit;
+import map.project.demo.Service.BillService;
+import map.project.demo.Service.UnitService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class LaborMap2ApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
 
+
+	}
 }
+
+
