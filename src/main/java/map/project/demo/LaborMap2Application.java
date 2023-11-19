33e165e23fda478305cad96fc11b054571ad74f4
andrei <andreiasnico@gmail.com>
2023-11-19 13:55:02 +0200

    mysql integrated in db

--- src/main/java/map/project/demo/LaborMap2Application.java
+++ src/main/java/map/project/demo/LaborMap2Application.java
@@ -2,6 +2,7 @@ package map.project.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @SpringBootApplication
